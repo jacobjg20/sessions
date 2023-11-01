8bfb03c6beb012561f1321120fdcea66fef9e205
jacobjg20 <35909673+jacobjg20@users.noreply.github.com>
2023-11-01 02:09:41 -0400

    Removed Tables Variable from dataBase.js

--- data-base/dataBase.js
+++ data-base/dataBase.js
@@ -1,10 +1,6 @@
 class DataBase{
     constructor(){
-        this.tables = {
-            jammy: 'https://scaling-trout-54rq4pv55r7f4jxx-3000.app.github.dev/jammy',
-            dirtcup: 'https://scaling-trout-54rq4pv55r7f4jxx-3000.app.github.dev/dirtcup'
-        };
-
+        
         this.users = [
             {
                 name: 'Jacob',
